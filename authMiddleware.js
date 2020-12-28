030080d9ebb238edfcdb408538f2a323236e3522
Kiran Hegde <kiranbanjagaric@gmail.com>
2020-12-28 11:39:10 +0530

    initial setup completed

---
+++ authMiddleware.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+const APP_SECRET = "myappsecret";
+const USERNAME = "admin";
+const PASSWORD = "secret";
+
+const mappings = {
+    get: ["/api/orders", "/orders"],
+    post: ["/api/products", "/products", "/api/categories", "/categories"]
+}
+
+function requiresAuth(method, url) {
+    return (mappings[method.toLowerCase()] || [])
+        .find(p => url.startsWith(p)) !== undefined;
+}
